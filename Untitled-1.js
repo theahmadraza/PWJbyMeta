9bce6e4bbcc6f7b932c3863450d10444e17d0107
Ahmad Raza <csmirza11@gmail.com>
2022-08-21 14:34:42 +0500

    first commit

---
+++ Untitled-1.js
@@ -0,0 +1,26 @@
+console.log("Ahmad")
+
+// ////////Object Literals with Bracket Notations ////////////////
+
+var house = {}
+
+house["rooms"] = 4;
+house["color"] = "Brown"
+house["price"] = 1234
+
+console.log(house)
+
+
+/////////// It can evaluate Expression
+
+var arrOfKeys = ["speed", "altitude", "color"]
+
+var drone = {
+    speed: 200,
+    altitude: 300,
+    color: "red"
+}
+
+for(var i = 0; i < arrOfKeys.length; i++){
+    console.log(drone[arrOfKeys[i]])
+}
